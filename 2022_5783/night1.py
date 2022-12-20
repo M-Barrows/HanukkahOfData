a4fa5d5f2a57d7d6e9539bbaa2611b7544a10c55
Michael Barrows <michael.barrows.a@gmail.com>
2022-12-19 23:41:43 -0500

    updated scripts to have comments

--- 2022_5783/night1.py
+++ 2022_5783/night1.py
@@ -2,11 +2,18 @@ import pandas as pd
 from pandasql import sqldf
 pysqldf = lambda q: sqldf(q, globals())
 
+#read in data
 customers = pd.read_csv("./data/noahs-customers.csv")
+
+#create first and last name columns
+#create last name len columns for easy comparison 
+#remove "-" from phone numbers
 customers[['first_name','last_name']]=customers['name'].str.split(' ', n= 1,expand=True)
 customers['last_name_len']=customers['last_name'].str.len()
 customers['phone_num']=customers['phone'].str.replace('-','')
 
+
+#Create t9 dialing lookup dict
 t9 = {
     2:['A','B','C'],
     3:['D','E','F'],
@@ -18,16 +25,17 @@ t9 = {
     9:['W','X','Y','Z']
 }
 
+#Filter to last names that are even divisions of phone number length (4,7,10)
 q = """
     SELECT a.last_name,a.phone,a.customerid,phone_num
     FROM customers a
     Where last_name_len in (4,7,10)
 """
 possible_matches=pysqldf(q)
-
 pm_dict = possible_matches.to_dict('records')
 comparrison_dict = {person['last_name']:[int(num) for num in person['phone_num'][-len(person['last_name']):]] for person in pm_dict}
 
+# Check each possible last name (letter by letter) against the t9 dictionary
 subset = []
 for name,phone_num in comparrison_dict.items():
     include_person = 1
@@ -37,10 +45,11 @@ for name,phone_num in comparrison_dict.items():
     if include_person == 1:
         subset.append({name:phone_num})
 
+# PI last name
 pi = list(subset[0].keys())[0]
 
+# Print PI phone number
 q = f"SELECT a.last_name,a.phone,a.customerid,phone_num FROM customers a Where last_name = '{pi}'"
-
 print(pysqldf(q)['phone'])
 
 
--- 2022_5783/night2.py
+++ 2022_5783/night2.py
@@ -2,13 +2,13 @@ import pandas as pd
 from pandasql import sqldf
 pysqldf = lambda q: sqldf(q, globals())
 
+# Load in all datasets 
 customers = pd.read_csv('data/noahs-customers.csv')
-
 orders_items = pd.read_csv('data/noahs-orders_items.csv')
-
 orders = pd.read_csv('data/noahs-orders.csv')
-
 products = pd.read_csv('data/noahs-products.csv')
+
+# Search for products with basic keywords
 print(products.loc[products['desc'].str.contains('coffee',False)])
 print(products.loc[ products['desc'].str.contains('bagel',False)])
 print(products.loc[ products['desc'].str.contains('clean',False)])
@@ -17,14 +17,18 @@ print(products.loc[ products['desc'].str.contains('clean',False)])
 # BKY5887 | Sesame Bagel
 # HOM8601 | Rug Cleaner
 
+#Filter orders_items dataset to only orders that contain 1 of the above items
 coffee_bagel_cleaner_order_items = orders_items.loc[orders_items['sku'].isin(['DLI1464','BKY4234','BKY5887','HOM8601'])]
-coffee_bagel_cleaner_order_items.head()
 
+#Group everything by order and place SKU quantity in each column
 possible_orders = pd.pivot(coffee_bagel_cleaner_order_items,index='orderid',columns='sku',values='qty')
 
+#Filter possible orders to those that bought coffee AND rug cleaner
 possible_orders.loc[(possible_orders['DLI1464']>0) & possible_orders['HOM8601']>0] # Orderid = 7409
 
+#Filter the customers dataset by the only order number returned above for the answer
 customer_orders = orders.join(customers.set_index('customerid'),on='customerid')
-customer_orders.loc[customer_orders['orderid']==7409]
+print(customer_orders.loc[customer_orders['orderid']==7409]['phone'])
+
 
 
